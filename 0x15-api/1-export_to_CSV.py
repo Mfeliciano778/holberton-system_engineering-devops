c2871d0738d8066f751153fdc94cbec2d6b6f0c7
Mfeliciano778 <1652@holbertonschool.com>
2021-05-03 19:16:50 +0000

    Task 1, completed??

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''takes an id for employee and return tasks completed'''
+import requests, sys, csv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    id = sys.argv[1]
+    employee = requests.get(url + 'users/' + id)
+    tasks = requests.get(url + 'todos/')
+
+    employee_dict = employee.json()
+    tasks_dict = tasks.json()
+
+    name = employee_dict['username']
+    filename = id + '.csv'
+
+    with open(filename, 'w+', newline="") as f:
+        write = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in tasks_dict:
+            if task['userId'] == int(id):
+                write.writerow([id, name, task['completed'], task['title']])
