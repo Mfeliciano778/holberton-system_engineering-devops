baad0c2035e90db981bcfcda87b3586fec93afa2
Mfeliciano778 <1652@holbertonschool.com>
2021-05-18 17:43:35 +0000

    Trying some stuff out

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,12 +9,9 @@ def top_ten(subreddit):
     amount = {"limit": 10}
     url = "https://www.reddit.com/r/" + subreddit + "/top.json"
 
+    r_sub = requests.get(url, headers=user_agent, params=amount,
+                            allow_redirects=False)
     try:
-        r_sub = requests.get(url, headers=user_agent, params=amount,
-                                allow_redirects=False)
-        if r_sub.status_code != 200:
-            print("None")
-            return
         r_sub = r_sub.json()
         for kid in r_sub.get("data").get("children"):
             print(kid.get("data").get("title"))
