7ae7df2b468ef04179d7541d797f62b46bca6d4c
Mfeliciano778 <1652@holbertonschool.com>
2021-05-18 15:07:27 +0000

    Task 1 done?

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''queries the Reddit API and returns the top ten posts a subreddit has'''
+import requests
+
+
+def top_ten(subreddit):
+    '''top_ten - get's top ten posts of a subreddit'''
+    user_agent = {"User-Agent": "somethingrandom"}
+    amount = {"limit": 10}
+    url = "https://www.reddit.com/r/" + subreddit + "/top.json"
+
+    try:
+        r_sub = requests.get(url, headers=user_agent, params=amount,
+                             allow_redirects=False).json()
+        for kid in r_sub.get("data").get("children"):
+            print(kid.get("data").get("title"))
+    except:
+        print("None")
